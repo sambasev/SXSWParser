2908b1261245220cdb62e8e67c76467c5f093457
Sambasevam Shanmugam <sambasevam@gmail.com>
2017-04-11 13:20:32 -0500

    Added parsing of Name/Email/Number, added findText(text, soup)

--- HelloWorldPy/src/SXSWParser.py
+++ HelloWorldPy/src/SXSWParser.py
@@ -11,24 +11,51 @@ from operator import itemgetter
 def hello(myInput):
     return "Hello World: " + myInput
 
+#Finds and prints text section in soup
+def findText(pText,soup):
+    tag = soup.find(text=pText)
+    if tag is not None and tag.parent is not None:
+        pTag = tag.parent.parent
+        
+        print("\n---FOUND TEXT---")
+        for sibling in pTag.findAll(text=True):
+            print(sibling.string)
+        print("---END----")
+    
+
 def parseLink(web_url):
     print "\nParsing "  + web_url.geturl()
     soup = BeautifulSoup(web_url.read(), "html.parser")
+    
     tag = soup.find(text="Public Film Contact")
     if tag is not None and tag.parent is not None:
         pTag = tag.parent.parent
-        print("\n---PUBLIC FILM CONTACT---\n")
-        for sibling in pTag.findAll(text=True):
-            print(sibling.string)
+        
+        print("\n---PUBLIC FILM CONTACT---")
+        pTag = pTag.find_next(text=True)
+        if pTag: print("Name  : " + pTag.find_next(text=True))
+        pTag = pTag.find_next(text=re.compile("@"))
+        if pTag: print("Email : " + pTag.string)
+        pTag = pTag.find_next(text=re.compile("[0-9]"))
+        if pTag: print("Number: " + pTag.string)
+        
+        print("---END----")
 
 #    for sibling in tag.next_siblings:
 #        print(sibling.string)
     tag = soup.find(text="Publicity Contact")
     if tag is not None and tag.parent is not None:
         pTag = tag.parent.parent
-        print("\n---PUBLICITY CONTACT---\n")
-        for sibling in pTag.findAll(text=True):
-            print(sibling.string)
+        
+        print("\n---PUBLICITY CONTACT---")
+        pTag = pTag.find_next(text=True)
+        if pTag: print("Name  : " + pTag.find_next(text=True))
+        pTag = pTag.find_next(text=re.compile("@"))
+        if pTag: print("Email : " + pTag.string)
+        pTag = pTag.find_next(text=re.compile("[0-9]"))
+        if pTag: print("Number: " + pTag.string)
+        
+        print("---END----")
       
     
 #myInput = raw_input("Please enter name:")
@@ -45,7 +72,7 @@ except urllib2.URLError :
 #print(soup.prettify())
 #Traverse site and get all music links
 L = []
-for link in soup.find_all('a', href=re.compile("films\/6")):
+for link in soup.find_all('a', href=re.compile("films\/65916")):
     dir_link = "http://schedule.sxsw.com" + link.get('href')
 #    print(link.get('href'))
     L.append(dir_link)
